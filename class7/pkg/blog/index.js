e1c0aedcbb3a5f5a889ceb60df7f2aa6df528125
Ljochev <ljochev@gmail.com>
2024-03-06 13:30:30 +0100

    class7WS

---
+++ class7/pkg/blog/index.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  user_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Account",
+  },
+  //   email: String,
+  title: String,
+  content: String,
+});
+
+const Posts = mongoose.model("Post", postSchema, "posts");
+
+// req.auth
+const getAll = async (user_id) => {
+  return await Posts.find({ user_id });
+};
+
+const getSingle = async (user_id, id) => {
+  return await Posts.find({ _id: id, user_id });
+};
+
+const getAllAlphabetically = async (user_id) => {
+  return await Posts.find({user_id}).sort({title: 1});
+};
+
+const create = async (data) => {
+  const post = new Posts(data);
+  return await post.save();
+};
+
+const update = async (id, data) => {
+  return await Posts.updateOne({ _id: id }, {data});
+};
+
+const remove = async (id) => {
+  return await Posts.deleteOne({ _id: id });
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  getAllAlphabetically,
+  create,
+  update,
+  remove,
+};
---
+++ class7/pkg/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { getSection } = require("../config");
+
+const { MONGO_USER_NAME, MONGO_PASSWORD } = getSection("development");
+
+const uri = `mongodb+srv://${MONGO_USER_NAME}:${MONGO_PASSWORD}@cluster0.coopvs9.mongodb.net/semos?retryWrites=true&w=majority&appName=Cluster0`
+
+async function connect() {
+  console.log(uri);
+  try {
+    await mongoose.connect(uri);
+    console.log("Mongoose is connected!");
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+connect();
